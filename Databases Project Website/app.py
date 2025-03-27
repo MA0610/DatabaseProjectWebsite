26333d92be061081088bb83c23266d548beae2aa
MA0610 <47308923+MA0610@users.noreply.github.com>
2025-03-27 11:01:25 -0700

    Add files via upload

---
+++ Databases Project Website/app.py
@@ -0,0 +1,36 @@
+from flask import Flask, request, jsonify, render_template, Blueprint  
+from models import db, Project
+
+app = Flask(__name__)
+
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///projects.db'  
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db.init_app(app)
+
+with app.app_context():
+    db.create_all()  # Creates the database tables
+
+
+
+# Sets home.html to / (root/start page)
+@app.route('/')
+def index():
+    return render_template('home.html')
+
+
+@app.route('/submitProject')
+def submit():
+    return render_template('submit.html')
+
+@app.route('/grabProject', methods=['GET'])  #NOT SETUP YET
+def getTutorial():
+    return jsonify()
+
+@app.route('/putProject',methods=['POST'])
+def putTutorial():
+    return jsonify(success=True, message="Tutorial added successfully")
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
