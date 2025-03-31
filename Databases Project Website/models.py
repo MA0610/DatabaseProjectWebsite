8559e5d54e351961c8f361ea2d140966dfb8278a
MA0610 <47308923+MA0610@users.noreply.github.com>
2025-03-30 22:02:49 -0700

    updated schema to include more info + changed parameters of attributes

--- Databases Project Website/models.py
+++ Databases Project Website/models.py
@@ -5,7 +5,8 @@ db = SQLAlchemy()
 
 class Project(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    userName = db.Column(db.String(20),unique=True,nullable=False)
-    text = db.Column(db.String(10000), nullable=False)
-    githubLink = db.Column(db.String(100), nullable=False)
+    userName = db.Column(db.String(20), nullable=False)
+    categories = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.String(10000), nullable=False)
+    githubLink = db.Column(db.String(100),nullable=False)
 
